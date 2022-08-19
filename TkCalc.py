acb392f9033336f23af2f4b84159d0e307d6914a
Lothar TheQuiet <lotharthequiet@gmail.com>
2022-08-18 23:48:27 -0400

    Lothar made yet ANOTHER code update.

--- TkCalc.py
+++ TkCalc.py
@@ -20,10 +20,15 @@ CRITICAL: Serious error
 
 from ast import Global
 import tkinter as tk
+from typing_extensions import Self
 import tksheet
 import logging
 
 from functools import partial
+from tkinter import Tk, ttk, Canvas
+from PIL import Image, ImageTk
+
+root = tk.Tk()
 
 class GlobalVars():
     shortname = "TkCalc"
@@ -31,10 +36,17 @@ class GlobalVars():
     appname = (shortname + " " + ver)
     btnsz = 2
     pad = 2
-    calcmem = None
+    calcmem = 0
     opmem = None
+    icon = "calculator.png"
+    author = "Lothar TheQuiet"
+    contact = "lotharthequiet@gmail.com"
+    codeassist = "Rilvyk"
+    git = "https://github.com/lotharthequiet/TkCalc.git"
+    display = tk.StringVar()
 
 class TkCalc():
+    GlobalVars.display.set = 0
     def mrc(mem=None):
         print("MRC")
 
@@ -47,12 +59,64 @@ class TkCalc():
     def memce(mem=None):
         print("CE")
         try:
-            GlobalVars.calcmem = None
+            GlobalVars.calcmem = 0
             print("Working memory cleared.")
             GlobalVars.opmem = None
             print("Operator memory cleared.")
+            TkCalc.usrentrylbl['text'] = GlobalVars.calcmem
         except Exception as e:
             print("Error clearing memory.")
+    
+    def exitapp():
+        TkCalc.root.quit()
+
+    def openabout():
+            aboutwindow = tk.Toplevel(root)
+            aboutwindow.title(GlobalVars.appname)
+            ico = Image.open(GlobalVars.icon)
+            photo = ImageTk.PhotoImage(ico)
+            aboutwindow.wm_iconphoto(False, photo)
+            aboutwinttlfrm = tk.LabelFrame(aboutwindow, text = "About")
+            aboutwinttlfrm.grid(column = 0, row = 0, padx = 5, pady = 5, sticky="NSEW")
+            img = Image.open(GlobalVars.icon)
+            img.resize((128, 128))
+            image = ImageTk.PhotoImage(img)
+            aboutcanvas = Canvas(aboutwinttlfrm, width=128, height=128)
+            aboutcanvas.create_image(128, 128, image = image)
+            aboutcanvas.grid(column=0, row=0, padx = 5, pady = 5, sticky="W")
+            aboutttllbl = tk.Label(aboutwinttlfrm, text=GlobalVars.appname)
+            aboutttllbl.grid(column=1, row=0, padx = 5, pady = 5, sticky="SW")
+            aboutauthorlbl = tk.Label(aboutwinttlfrm, text ="Written By:")
+            aboutauthorlbl.grid(column = 0, row = 1, padx = 5, pady = 5, sticky="W")
+            aboutauthor = tk.Label(aboutwinttlfrm, text = GlobalVars.author)
+            aboutauthor.grid(column = 1, row = 1, padx = 5, pady = 5, sticky="W")
+            aboutcontactlbl = tk.Label(aboutwinttlfrm, text ="Contact:")
+            aboutcontactlbl.grid(column = 0, row = 2, padx = 5, pady = 5, sticky="W")
+            aboutcontact = tk.Label(aboutwinttlfrm, text = GlobalVars.contact)
+            aboutcontact.grid(column = 1, row = 2, padx = 5, pady = 5, sticky="W")
+            aboutcodeasstlbl = tk.Label(aboutwinttlfrm, text ="Coding Assistance:")
+            aboutcodeasstlbl.grid(column = 0, row = 3, padx = 5, pady = 5, sticky="W")
+            aboutcodeasst = tk.Label(aboutwinttlfrm, text = GlobalVars.codeassist)
+            aboutcodeasst.grid(column = 1, row = 3, padx = 5, pady = 5, sticky="W")
+            aboutgitlbl = tk.Label(aboutwinttlfrm, text ="Git Link:")
+            aboutgitlbl.grid(column = 0, row = 4, padx = 5, pady = 5, sticky="W")
+            aboutgithubgit = tk.Label(aboutwinttlfrm, text = GlobalVars.git)
+            aboutgithubgit.grid(column = 1, row = 4, padx = 5, pady = 5, sticky="W")
+            aboutverlbl = tk.Label(aboutwinttlfrm, text = "Version:")
+            aboutverlbl.grid(column = 0, row = 5, padx = 5, pady = 5, sticky="W")
+            aboutver = tk.Label(aboutwinttlfrm, text = GlobalVars.ver)
+            aboutver.grid(column = 1, row = 5, padx = 5, pady = 5, sticky="W")
+
+    def openhelp():
+        helpwindow = tk.Toplevel(root)
+        helpwindow.title(GlobalVars.appname)
+        ico = Image.open(GlobalVars.icon)
+        photo = ImageTk.PhotoImage(ico)
+        helpwindow.wm_iconphoto(False, photo)
+        helpwinttlfrm = tk.LabelFrame(helpwindow, text = "Help")
+        helpwinttlfrm.grid(column = 0, row = 0, padx = 5, pady = 5, sticky="NSEW")
+        helpauthorlbl = tk.Label(helpwinttlfrm, text ="Written By:")
+        helpauthorlbl.grid(column= 0, row = 0, padx = 5, pady = 5, sticky="W")
 
     def docalc(value):
         print("DoCalc function.")
@@ -88,12 +152,18 @@ class TkCalc():
                     operation = 4
                 case ".":
                     operation = 5
+                case "=":
+                    operation = 6
 
             if operation == 0:
                 if not GlobalVars.calcmem:
                     GlobalVars.calcmem = value
+                    #Self.usrentrylbl['text'] = GlobalVars.calcmem
+                    TkCalc.usrentrylbl.configure(text=GlobalVars.calcmem)
                 else:
                     GlobalVars.calcmem = int(str(GlobalVars.calcmem) + str(value))
+                    #Self.usrentrylbl['text'] = GlobalVars.calcmem
+                    TkCalc.usrentrylbl.configure(text=GlobalVars.calcmem)
                     
             if operation == 5:
                 print(GlobalVars.calcmem)
@@ -126,10 +196,24 @@ class TkCalc():
     buttoneq = partial(docalc, "=")
     buttonperiod = partial(docalc, ".")
 
-    root = tk.Tk()
+    
     root.title(GlobalVars.appname)
     root.geometry("260x210")
-    usrentrylbl = tk.Entry(root, state="readonly", width=27, textvariable=GlobalVars.calcmem, readonlybackground='white')
+    ico = Image.open(GlobalVars.icon)
+    photo = ImageTk.PhotoImage(ico)
+    root.wm_iconphoto(False, photo)
+    nb = ttk.Notebook(root)
+    menubar = tk.Menu(nb)
+    filemenu = tk.Menu(menubar, tearoff=0)
+    filemenu.add_command(label="Exit", command=exitapp)
+    menubar.add_cascade(label="File", menu=filemenu)
+    helpmenu = tk.Menu(menubar, tearoff=0)                                     
+    helpmenu.add_command(label="Help", command=openhelp)
+    helpmenu.add_separator()
+    helpmenu.add_command(label="About", command=openabout)
+    menubar.add_cascade(label="Help", menu=helpmenu)
+    root.config(menu=menubar)
+    usrentrylbl = tk.Entry(root, state="readonly", width=27, textvariable=GlobalVars.display, readonlybackground='white')
     usrentrylbl.grid(column=0, row=0, columnspan=5, padx=GlobalVars.pad, pady=GlobalVars.pad)
     mrcbtn = tk.Button(root, text="MRC", width=GlobalVars.btnsz, command=mrc)
     mrcbtn.grid(column=0, row=1, padx=GlobalVars.pad, pady=GlobalVars.pad)
